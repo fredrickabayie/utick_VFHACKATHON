acfd049c164f5f4371e3aaf6d3eb7ecc7ad95cb6
kennethmensah <kenneth.mensah@ashesi.edu.gh>
2016-03-18 01:27:52 +0000

    Added function to add and retrieve a transaction

--- models/transactions.php
+++ models/transactions.php
@@ -16,10 +16,48 @@ class transactions extends adb_object{
 
     }
 
+    /**
+     * @param $total
+     * @param $user_email
+     * @return bool
+     */
+    function addReceipt($total, $user_email){
 
-    function addTransaction(){
+        $str_query = "INSERT INTO receipts
+                      SET
+                      total_cost = $total,
+                      user_email = $user_email,
+                      date = NOW()";
 
-        $str_query = "SELECT"
+        return $this->query($str_query);
+    }
+
+
+    function addTransaction($receipt_id, $event_id){
+
+        $str_query = "INSERT INTO transactions
+                      SET
+                      receipt_id = $receipt_id,
+                      event_id = $event_id";
+
+        return $this->query($str_query);
+    }
+
+
+    /**
+     * @param $receipt
+     */
+    function getTransaction($receipt){
+
+        $str_query = "SELECT * FROM
+                      transactions T INNER JOIN receipts R
+                      ON T.receipt_id = R.receipt_id
+                      INNER JOIN event E
+                      ON T.event_id = E.event_id
+                      INNER JOIN event_schedule ES
+                      ON E.event_id = ES.event_id
+                      WHERE T.receipt_id = $receipt";
+
     }
 
 
